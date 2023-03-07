c7cc1dccfa2cbb05f9acf84e87f0b0be308ad9a8
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-07 15:33:43 +0100

    exercise complete

---
+++ assets/Js/main.js
@@ -0,0 +1,37 @@
+
+/* 
+context: describing sequences: ask to user his name, 
+his surname, 
+his favourite colour, 
+at last show on the page the result '+23'
+using this writing format: namesurnamefavouritecolour23
+
+/* open the dialog window on the browser */
+
+ alert("main.js connect");
+ 
+ // request for his name
+
+ let userName = prompt("Qual'è il tuo Nome?");
+ console.log(userName);
+ 
+ //request for his surname
+
+ let userSurname;
+ userSurname = prompt("Qual'è il tuo Cognome?");
+ console.log(userSurname);
+ 
+ //request for his favourite colour
+
+ let usercolor;
+ usercolor = prompt("Qual'è il tuo Colore preferito?");
+ console.log(usercolor);
+ 
+ //show on the page the result adding '+23' at the end
+
+ let result = userName + userSurname + usercolor + 23;
+ console.log(result);
+
+ let element = document.getElementById("result_display");
+ console.log (element);
+ element.innerHTML = result;
